d8761325f07a6545e17be31fa578093134480069
drobash665 <AD10122004@mail.ru>
2026-03-18 12:10:54 +0500

    PrimeVue

--- VuePS/src/main.js
+++ VuePS/src/main.js
@@ -1,7 +1,9 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
 
 import App from './App.vue'
 import router from '@/router.js'
@@ -10,5 +12,14 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue,{
+  theme:{
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false
+    }
+  }
+});
 app.mount('#app')
